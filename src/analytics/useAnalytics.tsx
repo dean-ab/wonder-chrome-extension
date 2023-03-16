6bc491155a2275ecdc8d7de886ab234b73e76b5e
dean-ab <deanabargil@gmail.com>
2023-03-16 19:00:29 +0200

    fixed some analytics stuff

--- src/analytics/useAnalytics.tsx
+++ src/analytics/useAnalytics.tsx
@@ -1,19 +1,28 @@
+import React, { createContext, useContext } from 'react';
+import { AnalyticsService, MockAnalyticsService } from './AnalyticsService';
 
-import React, { createContext, useContext } from "react";
-import { AnalyticsService } from "./AnalyticsService";
+const IS_DEV = import.meta.env.DEV;
 
 const AnalyticsContext = createContext<AnalyticsService | null>(null);
 
-export const AnalyticsProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return <AnalyticsContext.Provider value={new AnalyticsService()}>{children}</AnalyticsContext.Provider>
-}
+export const AnalyticsProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <AnalyticsContext.Provider
+      value={IS_DEV ? new MockAnalyticsService() : new AnalyticsService()}
+    >
+      {children}
+    </AnalyticsContext.Provider>
+  );
+};
 
 export const useAnalytics = () => {
-    const analytics = useContext(AnalyticsContext);
+  const analytics = useContext(AnalyticsContext);
 
-    if (!analytics) {
-        throw new Error("AnalyticsService is not initialized");
-    }
+  if (!analytics) {
+    throw new Error('AnalyticsService is not initialized');
+  }
 
-    return analytics;
-}
+  return analytics;
+};
