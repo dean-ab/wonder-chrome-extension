497081cc69197fd23f840ad1f74bb05a665aa07c
dean-ab <deanabargil@gmail.com>
2023-03-16 19:00:20 +0200

    Fixed some analytics stuff

--- src/analytics/AnalyticsService.ts
+++ src/analytics/AnalyticsService.ts
@@ -3,19 +3,42 @@ import { Events } from './Events';
 
 const API_KEY = '4d50fa3ebdaf39596c1105808e059777';
 
+interface IAnalyticsService {
+  track(
+    eventName: Events,
+    props?: Record<string, any>,
+    optionsOrCallback?: RequestOptions | Callback,
+    callback?: Callback,
+  ): void;
+}
 
-export class AnalyticsService {
-    constructor() {
-        mixpanel.init(API_KEY, { debug: true, api_host: "https://api.mixpanel.com" });
-    }
+export class AnalyticsService implements IAnalyticsService {
+  constructor() {
+    mixpanel.init(API_KEY, {
+      debug: true,
+      api_host: 'https://api.mixpanel.com',
+    });
+  }
 
-    public track(
-        eventName: Events,
-        props?: Record<string, any>,
-        optionsOrCallback?: RequestOptions | Callback,
-        callback?: Callback,
-    ): void {
-        console.log("Mixpanel", { eventName, props });
-        // return mixpanel.track(eventName, props, optionsOrCallback, callback);
-    }
+  public track(
+    eventName: Events,
+    props?: Record<string, any>,
+    optionsOrCallback?: RequestOptions | Callback,
+    callback?: Callback,
+  ): void {
+    console.log();
+    // console.log("Mixpanel", { eventName, props });
+    return mixpanel.track(eventName, props, optionsOrCallback, callback);
+  }
+}
+
+export class MockAnalyticsService implements IAnalyticsService {
+  track(
+    eventName: Events,
+    props?: Record<string, any>,
+    optionsOrCallback?: any,
+    callback?: any,
+  ): void {
+    console.log('Mock Analytics Service', { eventName, props });
+  }
 }
